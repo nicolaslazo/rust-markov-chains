9fa22e225694c3d6489f948415a2623c8c205b3f
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2022-02-08 10:19:23 -0300

    Iterates over token transitions ignoring spaces

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use regex::{Match, Matches, Regex};
 use std::{fs, str};
 
-struct TokenTransitions<'t, 'r> {
+struct RegexInclusiveSplit<'t, 'r> {
     text: &'t str,
     it: Matches<'r, 't>,
     last_match_consumed: Option<Match<'t>>,
     reported_index: usize,
 }
 
-impl<'t, 'r> TokenTransitions<'t, 'r> {
+impl<'t, 'r> RegexInclusiveSplit<'t, 'r> {
     fn new(text: &'t str, pattern: &'r Regex) -> Self {
         let mut it = pattern.find_iter(text);
         let last_match_consumed = it.next();
-        TokenTransitions {
+        RegexInclusiveSplit {
             text,
             it,
             last_match_consumed,
@@ -21,28 +21,23 @@ impl<'t, 'r> TokenTransitions<'t, 'r> {
     }
 }
 
-impl<'t, 'r> Iterator for TokenTransitions<'t, 'r> {
-    type Item = (&'t str, &'t str);
+impl<'t, 'r> Iterator for RegexInclusiveSplit<'t, 'r> {
+    type Item = &'t str;
 
-    fn next(&mut self) -> Option<Self::Item> {  // I'm sure there's something wonky happening at the end of the input text
+    fn next(&mut self) -> Option<Self::Item> {
         if let Some(regex_match) = self.last_match_consumed {
             let last_match_consumed_start = regex_match.start();
             if self.reported_index < last_match_consumed_start {
-                // ltoken in between regex matches
-                let ltoken = &self.text[self.reported_index..last_match_consumed_start];
+                // In between separators
+                let retval = &self.text[self.reported_index..last_match_consumed_start];
                 self.reported_index = last_match_consumed_start;
-		let rtoken = regex_match.as_str();
-                return Some((ltoken, rtoken));
+                return Some(retval);
             } else if self.reported_index == last_match_consumed_start {
-                // ltoken is a regex match
-                let ltoken = &regex_match.as_str();
+                // On a separator
+                let retval = &regex_match.as_str();
                 self.reported_index = regex_match.end();
                 self.last_match_consumed = self.it.next();
-		if let Some(new_regex_match) = self.last_match_consumed {
-		    let rtoken = &self.text[self.reported_index..new_regex_match.start()];
-		    return Some((ltoken, rtoken));
-		}
-		else { None }  // We reached the end of the input text?
+                return Some(retval);
             } else {
                 panic!("Should be unreachable");
             }
@@ -52,11 +47,41 @@ impl<'t, 'r> Iterator for TokenTransitions<'t, 'r> {
     }
 }
 
+struct TokenTransitions<'t, 'r> {
+    token_it: &'t mut RegexInclusiveSplit<'t, 'r>,
+    last_token: &'t str,
+}
+
+impl<'t, 'r> TokenTransitions<'t, 'r> {
+    fn new(token_it: &'t mut RegexInclusiveSplit<'t, 'r>) -> Self {
+	let last_token = token_it.next().unwrap();
+	TokenTransitions {
+	    token_it,
+	    last_token
+	}
+    }
+}
+	
+impl<'t, 'r> Iterator for TokenTransitions<'t, 'r> {
+    type Item = (&'t str, &'t str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+	while let Some(next_token) = self.token_it.next() {
+	    if next_token == " " { continue };  // We're ignoring other tokens with spaces like ". " for now
+
+	    let retval = (self.last_token, next_token);
+	    self.last_token = next_token;
+	    return Some(retval)
+	}
+	return None
+    }
+}
+
 fn main() {
     let input_text = fs::read_to_string("neuromancer.txt").unwrap();
     let token_delimiter_re = Regex::new(r#"(([\.,:]?( |\t|\n)+)|--|")"#).unwrap();
-
-    for transition in TokenTransitions::new(&input_text, &token_delimiter_re) {
+    let mut token_it = RegexInclusiveSplit::new(&input_text, &token_delimiter_re);
+    for transition in TokenTransitions::new(&mut token_it) {
         println!("{:?}", transition);
     }
 }
